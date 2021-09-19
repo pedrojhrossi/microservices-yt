613f52d2585f08560d1a9b2eca2f91d4e8bf9748
PJHR <pedrojhrossi@gmail.com>
2021-09-18 20:12:31 -0400

    First commit, base services to start with spring cloud and microservices.

---
+++ bike-service/src/main/java/com/tutorial/bikeservice/controller/BikeController.java
@@ -0,0 +1,54 @@
+package com.tutorial.bikeservice.controller;
+
+import com.tutorial.bikeservice.entity.Bike;
+import com.tutorial.bikeservice.service.BikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bike")
+public class BikeController {
+
+    @Autowired
+    BikeService bikeService;
+
+    @GetMapping
+    public ResponseEntity<List<Bike>> getAll() {
+        List<Bike> bikes = bikeService.getAll();
+        if (bikes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(bikes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Bike> getBikeById(@PathVariable("id") int id) {
+        Bike bike = bikeService.getBikeById(id);
+        if (bike == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(bike);
+    }
+
+    @PostMapping
+    public ResponseEntity<Bike> save(@RequestBody Bike bike) {
+        Bike bikeNew = bikeService.save(bike);
+        return ResponseEntity.ok(bikeNew);
+    }
+
+    @GetMapping("/byuser/{userId}")
+    public ResponseEntity<List<Bike>> getByUserId(@PathVariable("userId") int userId) {
+        List<Bike> bikes = bikeService.getByUserId(userId);
+        if (bikes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(bikes);
+    }
+
+}
